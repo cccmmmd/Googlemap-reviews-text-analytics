497cc72199c939aec17e3b737827cbaafbfb6370
cccmmmd <bblisme8b@gmail.com>
2024-02-16 23:30:33 +0800

    seperate two string

--- web.py
+++ web.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 
 token = ''
 data = []
+data2 = []
 page = 1
 
 def get_20_reviews():
@@ -14,7 +15,10 @@ def get_20_reviews():
             get_reviews_data(tkn=token)
         page += 1
     return data
-
+def remove_common(a, b):
+    a, b = list(set(a).difference(b)), list(set(b).difference(a))
+ 
+    
 def get_reviews_data(tkn):
     global token
     headers = {
@@ -31,15 +35,34 @@ def get_reviews_data(tkn):
     for el in soup.select('.gws-localreviews__google-review'):
         node = el.select_one('.f5axBf > span')
         if node is not None:
-            tempreview = "___".join(node.text.split('|'))
+           tempreview = node.get_text(separator=' ').split()
         else:
-            tempreview = None
+           tempreview = None
         data.append(tempreview)
 
+        node2 = el.select_one('.k8MTF')
+        if node2 is not None:
+           tempreview2 = node2.get_text(separator=' ').split()
+        else:
+           tempreview2 = None
+        data2.append(tempreview2)
+
+    
+    #print(data)
+    #print(data2)
+
+    for d1 in data:
+        for d2 in data2:
+            #for ds2 in d2:
+            print(d1, d2)
+            
   
     for user_data in data:
         print(user_data)
+
+    for user_data in data2:
+       print(user_data)
     
-    #return data
+    return data
 
 get_20_reviews()
